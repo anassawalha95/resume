f599948d030970f628d1a030207272b1c8d4a6fc
anas.swalha <anas.swalha@yahoo.com>
2020-07-21 20:32:35 +0300

    Initial commit

---
+++ precache-manifest.078e535c44ae9ce634a6b088bc73e8a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cf77a76a1a3dd8bcdf5c838ed07cd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d449c1b2c056c280e1",
+    "url": "/static/css/main.75f92da6.chunk.css"
+  },
+  {
+    "revision": "5e026c3088af6abe07a8",
+    "url": "/static/js/2.6b66dfd3.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.6b66dfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d449c1b2c056c280e1",
+    "url": "/static/js/main.0b9a0b40.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "ad68dcd5a23aabf886d47842d11544d7",
+    "url": "/static/media/jsDoc.ad68dcd5.png"
+  },
+  {
+    "revision": "9aeae2dc576be30d75cc176257e94e8f",
+    "url": "/static/media/markededitro.9aeae2dc.png"
+  },
+  {
+    "revision": "485b0e21fce0de8188f533eafa300b30",
+    "url": "/static/media/productPromo.485b0e21.png"
+  },
+  {
+    "revision": "8c0242fa30aa30bc6309b4d32a17599b",
+    "url": "/static/media/quoteM.8c0242fa.png"
+  }
+]);
